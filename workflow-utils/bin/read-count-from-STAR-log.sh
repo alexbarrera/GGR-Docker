2939e84224fcd4a5dc95fb5def2e35f488bb7962
Alejandro Barrera <alejandro.barrera@duke.edu>
2016-10-06 11:22:21 -0400

    Add number of reads mapped to multiple loci

--- workflow-utils/bin/read-count-from-STAR-log.sh
+++ workflow-utils/bin/read-count-from-STAR-log.sh
@@ -6,5 +6,6 @@ if [ "$#" -ne 1 ]; then
 fi
 
 reads_processed=$(($(grep 'Number of input reads' $1 | cut -f 2) * 2))
-reads_mapped=$(($(grep "Uniquely mapped reads number" $1 | cut -f 2 )  * 2))
-echo -e $reads_processed"\t"$reads_mapped
+reads_mapped_uniquely=$(($(grep "Uniquely mapped reads number" $1 | cut -f 2 )  * 2))
+reads_mapped_multiply=$(($(grep "Number of reads mapped to multiple loci" $1 | cut -f 2 )  * 2))
+echo -e $reads_processed"\t"$reads_mapped_uniquely"\t"$reads_mapped_multiply
